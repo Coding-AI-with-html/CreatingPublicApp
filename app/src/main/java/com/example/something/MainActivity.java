f9482d95a9407f865357f35915385ecc072c1f57
Coding-AI-with-html <mankuspijus@gmail.com>
2020-09-20 22:20:44 +0300

    Creating app from zero, starting with login system

---
+++ app/src/main/java/com/example/something/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.something;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainscreen);
+    }
+
+    /**
+     * Scan user local data if he had already acc, if not, go to register, if logged out, go loggin system, if still logged stay at main activity
+     *
+     */
+
+
+
+}
